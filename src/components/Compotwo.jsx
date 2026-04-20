c5c6dd5a1fcb245e74b4cefa363ca9b624141443
MohamedG23 <yourname@email.com>
2026-04-20 15:25:08 +0300

    newone

--- src/components/Compotwo.jsx
+++ src/components/Compotwo.jsx
@@ -1,10 +1,20 @@
 import { IoIosAddCircle } from "react-icons/io";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import axios from "axios";
 
 export default function Compotwo() {
-  const [isHovered, setIsHovered] = useState(false);
+  const [proudect , setproudect] = useState([])
 
+  useEffect(() => {
+    let domain = "http://localhost:1337";
+    let endpoint = "/api/prodacts";
+    let url = domain + endpoint;
+    axios.get(url, { params: { populate: "*" } }).then((res) => {
+      console.log(res.data.data);
+      setproudect(res.data.data)
+    });
+  }, []);
   return (
     <div className="px-7 py-20 font-myfont grid grid-cols-1 md:grid-cols-4 gap-5">
       <div className="flex justify-center flex-col gap-8">
@@ -20,61 +30,35 @@ export default function Compotwo() {
           Explor
         </a>
       </div>
-      <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-        <img className="w-[300px]" src="product-1.png" alt="img" />
-        <div>
-          <p className="font-bold">Nordic Chair</p>
-          <h3 className="font-bold">$50000</h3>
-        </div>
-        <div className="bg-gray-400">
-          <IoIosAddCircle />
-        </div>
-      </div>
-      <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-        <img className="w-[300px]" src="product-2.png" alt="img" />
-        <div>
-          <p className="font-bold">Kruzo Aero Chair</p>
-          <h3 className="font-bold">$78000</h3>
-        </div>
-        <div className="bg-gray-400">
-          <IoIosAddCircle />
-        </div>
-      </div>
-      <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-        <img className="w-[300px]" src="product-3.png" alt="img" />
-        <div>
-          <p className="font-bold">Ergonomic Chair</p>
-          <h3 className="font-bold">$43000</h3>
-        </div>
-        <div className="bg-gray-400">
-          <IoIosAddCircle />
-        </div>
-      </div>
 
-      <div className="relative flex flex-col items-center group cursor-pointer w-64 pt-10">
-        {/* صورة الكرسي - نستخدم z-10 للتأكد أنها فوق الخلفية */}
-        <img
-          src="product-1.png"
-          alt="Nordic Chair"
-          className="w-48 z-10 transition-transform duration-300 group-hover:-translate-y-4"
-        />
+      {proudect?.map((el) => (
+        <div key={el.documentId} className="relative flex flex-col items-center group cursor-pointer w-64 pt-10">
+          {/* صورة الكرسي - نستخدم z-10 للتأكد أنها فوق الخلفية */}
+          <img
+            src={'http://localhost:1337' + el?.coverimg.url}
+            alt="Nordic Chair"
+            className="w-48 z-10 transition-transform duration-300 group-hover:-translate-y-4"
+          />
 
-        {/* خلفية الكارت التي تظهر عند الـ Hover */}
-        <div className="absolute bottom-0 left-0 right-0 h-0 bg-gray-200/50 rounded-2xl transition-all duration-300 group-hover:h-[75%] -z-0"></div>
+          {/* خلفية الكارت التي تظهر عند الـ Hover */}
+          <div className="absolute bottom-0 left-0 right-0 h-0 bg-gray-200/50 rounded-2xl transition-all duration-300 group-hover:h-[75%] -z-0"></div>
 
-        {/* نصوص المنتج */}
-        <div className="text-center mt-4 z-10">
-          <h3 className="text-gray-800 font-semibold text-lg">Nordic Chair</h3>
-          <p className="text-gray-900 font-bold text-xl mt-1">$50.00</p>
+          {/* نصوص المنتج */}
+          <div className="text-center mt-4 z-10">
+            <h3 className="text-gray-800 font-semibold text-lg">
+              {el.name}
+            </h3>
+            <p className="text-gray-900 font-bold text-xl mt-1">${el.price}</p>
+          </div>
+
+          {/* زر الإضافة (يظهر فقط عند الـ Hover) */}
+          <div className="absolute bottom-[-20px] opacity-0 transition-all duration-300 bg-gray-700/5 rounded-2xl h-50 w-50 flex justify-center items-end group-hover:opacity-100 group-hover:bottom-[-25px] z-20">
+            <button className="bg-gray-800 rounded-full p-3 shadow-lg">
+              <IoIosAddCircle className="h-8 w-8 text-white bg-gray-800" />
+            </button>
+          </div>
         </div>
-
-        {/* زر الإضافة (يظهر فقط عند الـ Hover) */}
-        <div className="absolute bottom-[-20px] opacity-0 transition-all duration-300 bg-gray-700/5 rounded-2xl h-50 w-50 flex justify-center items-end group-hover:opacity-100 group-hover:bottom-[-25px] z-20">
-          <button className="bg-gray-800 rounded-full p-3 shadow-lg">
-            <IoIosAddCircle className="h-8 w-8 text-white bg-gray-800" />
-          </button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,79 +1,175 @@
-import Footer from "./Footer";
+import axios from "axios";
+import { div } from "motion/react-client";
+import { useEffect, useState } from "react";
+import { IoIosAddCircle } from "react-icons/io";
 
 export default function Shop() {
+  const [proudect, setproudect] = useState([]);
+
+  useEffect(() => {
+    let domain = "http://localhost:1337";
+    let endpoint = "/api/prodacts";
+    let url = domain + endpoint;
+    axios.get(url, { params: { populate: "*" } }).then((res) => {
+      console.log(res.data.data);
+      setproudect(res.data.data);
+    });
+  }, []);
   return (
     <div>
-        <div  className="bg-mycolor px-5 gap-4 flex items-center h-40 md:h-80">
+      <div className="bg-mycolor px-5 gap-4 flex items-center h-40 md:h-80">
         <h1 className="text-4xl font-myfont font-bold text-white">Shop</h1>
+      </div>
+
+      {/* <div className="grid gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 py-25">
+        <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
+          <img className="w-[300px]" src="product-1.png" alt="img" />
+          <div>
+            <p className="font-bold">Nordic Chair</p>
+            <h3 className="font-bold">$50000</h3>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
+          <img className="w-[300px]" src="product-2.png" alt="img" />
+          <div>
+            <p className="font-bold">Kruzo Aero Chair</p>
+            <h3 className="font-bold">$78000</h3>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
+          <img className="w-[300px]" src="product-3.png" alt="img" />
+          <div>
+            <p className="font-bold">Ergonomic Chair</p>
+            <h3 className="font-bold">$43000</h3>
+          </div>
         </div>
 
-
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 py-25">
-                    <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-                      <img className="w-[300px]" src="product-1.png" alt="img" />
-                      <div>
-                          <p className="font-bold">Nordic Chair</p>
-                          <h3 className="font-bold">$50000</h3>
-                      </div>
-
-                    </div>
-                    <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-                      <img className="w-[300px]" src="product-2.png" alt="img" />
-                      <div>
-                          <p className="font-bold">Kruzo Aero Chair</p>
-                          <h3 className="font-bold">$78000</h3>
-                      </div>
-
-                    </div>
-                    <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-                      <img className="w-[300px]" src="product-3.png" alt="img" />
-                      <div>
-                          <p className="font-bold">Ergonomic Chair</p>
-                          <h3 className="font-bold">$43000</h3>
-                      </div>
-                    </div>
-
-                    <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-                      <img className="w-[300px]" src="product-1.png" alt="img" />
-                      <div>
-                          <p className="font-bold">Nordic Chair</p>
-                          <h3 className="font-bold">$50000</h3>
-                      </div>
-
-                    </div>
-                    <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-                      <img className="w-[300px]" src="product-2.png" alt="img" />
-                      <div>
-                          <p className="font-bold">Kruzo Aero Chair</p>
-                          <h3 className="font-bold">$78000</h3>
-                      </div>
-
-                    </div>
-                    <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-                      <img className="w-[300px]" src="product-3.png" alt="img" />
-                      <div>
-                          <p className="font-bold">Ergonomic Chair</p>
-                          <h3 className="font-bold">$43000</h3>
-                      </div>
-                    </div>
-
-                                        <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-                      <img className="w-[300px]" src="product-1.png" alt="img" />
-                      <div>
-                          <p className="font-bold">Nordic Chair</p>
-                          <h3 className="font-bold">$50000</h3>
-                      </div>
-
-                    </div>
-                    <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
-                      <img className="w-[300px]" src="product-2.png" alt="img" />
-                      <div>
-                          <p className="font-bold">Kruzo Aero Chair</p>
-                          <h3 className="font-bold">$78000</h3>
-                      </div>
-                    </div>
+        <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
+          <img className="w-[300px]" src="product-1.png" alt="img" />
+          <div>
+            <p className="font-bold">Nordic Chair</p>
+            <h3 className="font-bold">$50000</h3>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
+          <img className="w-[300px]" src="product-2.png" alt="img" />
+          <div>
+            <p className="font-bold">Kruzo Aero Chair</p>
+            <h3 className="font-bold">$78000</h3>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
+          <img className="w-[300px]" src="product-3.png" alt="img" />
+          <div>
+            <p className="font-bold">Ergonomic Chair</p>
+            <h3 className="font-bold">$43000</h3>
+          </div>
         </div>
 
+        <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
+          <img className="w-[300px]" src="product-1.png" alt="img" />
+          <div>
+            <p className="font-bold">Nordic Chair</p>
+            <h3 className="font-bold">$50000</h3>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 items-center hover:bg-gray-500 hover:rounded">
+          <img className="w-[300px]" src="product-2.png" alt="img" />
+          <div>
+            <p className="font-bold">Kruzo Aero Chair</p>
+            <h3 className="font-bold">$78000</h3>
+          </div>
+        </div>
+      </div> */}
+
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-3 py-25">
+        {proudect?.map((el) => (
+          <div className="relative flex flex-col items-center group cursor-pointer w-64 pt-10">
+            {/* صورة الكرسي - نستخدم z-10 للتأكد أنها فوق الخلفية */}
+            <img
+              src={"http://localhost:1337" + el?.coverimg.url}
+              alt="Nordic Chair"
+              className="w-48 z-10 transition-transform duration-300 group-hover:-translate-y-4"
+            />
+
+            {/* خلفية الكارت التي تظهر عند الـ Hover */}
+            <div className="absolute bottom-0 left-0 right-0 h-0 bg-gray-200/50 rounded-2xl transition-all duration-300 group-hover:h-[75%] -z-0"></div>
+
+            {/* نصوص المنتج */}
+            <div className="text-center mt-4 z-10">
+              <h3 className="text-gray-800 font-semibold text-lg">{el.name}</h3>
+              <p className="text-gray-900 font-bold text-xl mt-1">
+                ${el.price}
+              </p>
+            </div>
+
+            {/* زر الإضافة (يظهر فقط عند الـ Hover) */}
+            <div className="absolute bottom-[-20px] opacity-0 transition-all duration-300 bg-gray-700/5 rounded-2xl h-50 w-50 flex justify-center items-end group-hover:opacity-100 group-hover:bottom-[-25px] z-20">
+              <button className="bg-gray-800 rounded-full p-3 shadow-lg">
+                <IoIosAddCircle className="h-8 w-8 text-white bg-gray-800" />
+              </button>
+            </div>
+          </div>
+        ))}
+
+        {proudect?.map((el) => (
+          <div className="relative flex flex-col items-center group cursor-pointer w-64 pt-10">
+            {/* صورة الكرسي - نستخدم z-10 للتأكد أنها فوق الخلفية */}
+            <img
+              src={"http://localhost:1337" + el?.coverimg.url}
+              alt="Nordic Chair"
+              className="w-48 z-10 transition-transform duration-300 group-hover:-translate-y-4"
+            />
+
+            {/* خلفية الكارت التي تظهر عند الـ Hover */}
+            <div className="absolute bottom-0 left-0 right-0 h-0 bg-gray-200/50 rounded-2xl transition-all duration-300 group-hover:h-[75%] -z-0"></div>
+
+            {/* نصوص المنتج */}
+            <div className="text-center mt-4 z-10">
+              <h3 className="text-gray-800 font-semibold text-lg">{el.name}</h3>
+              <p className="text-gray-900 font-bold text-xl mt-1">
+                ${el.price}
+              </p>
+            </div>
+
+            {/* زر الإضافة (يظهر فقط عند الـ Hover) */}
+            <div className="absolute bottom-[-20px] opacity-0 transition-all duration-300 bg-gray-700/5 rounded-2xl h-50 w-50 flex justify-center items-end group-hover:opacity-100 group-hover:bottom-[-25px] z-20">
+              <button className="bg-gray-800 rounded-full p-3 shadow-lg">
+                <IoIosAddCircle className="h-8 w-8 text-white bg-gray-800" />
+              </button>
+            </div>
+          </div>
+        ))}
+
+        {proudect?.map((el) => (
+          <div className="relative flex flex-col items-center group cursor-pointer w-64 pt-10">
+            {/* صورة الكرسي - نستخدم z-10 للتأكد أنها فوق الخلفية */}
+            <img
+              src={"http://localhost:1337" + el?.coverimg.url}
+              alt="Nordic Chair"
+              className="w-48 z-10 transition-transform duration-300 group-hover:-translate-y-4"
+            />
+
+            {/* خلفية الكارت التي تظهر عند الـ Hover */}
+            <div className="absolute bottom-0 left-0 right-0 h-0 bg-gray-200/50 rounded-2xl transition-all duration-300 group-hover:h-[75%] -z-0"></div>
+
+            {/* نصوص المنتج */}
+            <div className="text-center mt-4 z-10">
+              <h3 className="text-gray-800 font-semibold text-lg">{el.name}</h3>
+              <p className="text-gray-900 font-bold text-xl mt-1">
+                ${el.price}
+              </p>
+            </div>
+
+            {/* زر الإضافة (يظهر فقط عند الـ Hover) */}
+            <div className="absolute bottom-[-20px] opacity-0 transition-all duration-300 bg-gray-700/5 rounded-2xl h-50 w-50 flex justify-center items-end group-hover:opacity-100 group-hover:bottom-[-25px] z-20">
+              <button className="bg-gray-800 rounded-full p-3 shadow-lg">
+                <IoIosAddCircle className="h-8 w-8 text-white bg-gray-800" />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
